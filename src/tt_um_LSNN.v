b3542430413142b110fcfb48f9fb0a2da2ae7cc6
Skye Gunasekaran <akgunase@ucsc.edu>
2023-11-02 20:28:12 -0700

    another one

--- src/tt_um_LSNN.v
+++ src/tt_um_LSNN.v
@@ -26,7 +26,6 @@ reg [7:0] next_state;
 reg [7:0] adaptation;    
 reg [7:0] threshold;
 reg [7:0] state;
-reg check;
 
 // clock cycle moves up state
 always @(posedge clk or posedge rst_n) begin
@@ -44,9 +43,9 @@ assign uo_out = (state >= threshold) ? 8'b00000001 : 8'b00000000;
 
 // Update the next state and threshold decay/increase
 always @(posedge clk) begin
-    next_state <= ui_in + (state >> 1); // decay by 50%
-    adaptation <= (state >= threshold) ? ((adaptation) + (adaptation >> 2)) : ((adaptation >> 1) + (adaptation >> 2)); // 25% increase or decrease
-    threshold <= b0j + adaptation;
+    next_state = ui_in + (state >> 1); // decay by 50%
+    adaptation = (state >= threshold) ? ((adaptation) + (adaptation >> 2)) : ((adaptation >> 1) + (adaptation >> 2)); // 25% increase or decrease
+    threshold = b0j + adaptation;
 end
 // Make threshold viewable
 assign uio_out = threshold;
