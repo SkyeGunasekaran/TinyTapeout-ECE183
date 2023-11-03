be7c43aa3a2d10633c4f38fe3eeec9d076b33c2b
Skye Gunasekaran <akgunase@ucsc.edu>
2023-11-02 19:54:39 -0700

    another one

--- src/tt_um_LSNN.v
+++ src/tt_um_LSNN.v
@@ -43,7 +43,7 @@ end
 assign uo_out = (state >= threshold) ? 8'b00000001 : 8'b00000000;
 
 // Update the next state and threshold decay/increase
-always @(*) begin
+always @(negedge clk) begin
     next_state <= ui_in + (state >> 1); // decay by 50%
     adaptation <= (state >= threshold) ? ((adaptation) + (adaptation >> 2)) : ((adaptation >> 1) + (adaptation >> 2)); // 25% increase or decrease
 end
