aaa9fa9f82e5a59427fa3e5c8b661e9c921c63f9
Skye Gunasekaran <akgunase@ucsc.edu>
2023-11-02 22:33:53 -0700

    another one

--- src/test_tb.v
+++ src/test_tb.v
@@ -2,54 +2,55 @@
 `timescale 1s/1s
 
 module LSNN_tb;
-reg        clk;
-reg        rst_n;
-reg  [7:0] ui_in;
-wire [7:0] uo_out;
-wire [7:0] uio_out;
+    reg        clk;
+    reg        rst_n;
+    reg  [7:0] ui_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
 
-tt_um_LSNN uut (
-    .clk(clk),
-    .rst_n(rst_n),
-    .ui_in(ui_in),
-    .uo_out(uo_out),
-    .uio_out(uio_out)
-);
+    tt_um_LSNN uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_out(uio_out)
+    );
 
-// Clock generation
-always begin
-    #5 clk = ~clk; // Toggle the clock every 5 time units
-end
+    // Clock generation
+    always begin
+        #1 clk = 1; // Toggle the clock for the first posedge at time 1
+        #1 clk = 0; // Toggle the clock back
+        #6 clk = 1; // Toggle the clock for the second posedge at time 8
+        #1 clk = 0; // Toggle the clock back
+    end
 
-initial begin
+    initial begin
+        // Initialize inputs
+        clk = 0;
+        rst_n = 1;
+        ui_in = 8'h00;
+        #5 rst_n = 0;
+        rst_n = 0;
 
-    // Initialize inputs
-    clk = 0;
-    rst_n = 1;
-    ui_in = 8'h00;
-    #5 rst_n = 0;
-    rst_n = 0;
+        #10 ui_in = 8'h10; // Spike up
+        #10 ui_in = 8'h20; // Spike up
+        #10 ui_in = 8'h10; // Spike up
+        #10 ui_in = 8'h00; // Spike down
+        #10 ui_in = 8'h10; // Spike up
+        #10 ui_in = 8'h99; // Spike up
+        #10 ui_in = 8'h00; // Spike down
+        #10 ui_in = 8'h00; // Spike down
+        #10 ui_in = 8'h10; // Spike up
+        #10 ui_in = 8'h10; // Spike up
+        #10 ui_in = 8'h00; // Spike down
 
-    #10 ui_in = 8'h10; // Spike up
-    #10 ui_in = 8'h20; // Spike up
-    #10 ui_in = 8'h10; // Spike up
-    #10 ui_in = 8'h00; // Spike down
-    #10 ui_in = 8'h10; // Spike up
-    #10 ui_in = 8'h99; // Spike up
-    #10 ui_in = 8'h00; // Spike down
-    #10 ui_in = 8'h00; // Spike down
-    #10 ui_in = 8'h10; // Spike up
-    #10 ui_in = 8'h10; // Spike up
-    #10 ui_in = 8'h00; // Spike down
+        $finish;
+    end
 
-    $finish;
-end
-
-
-initial begin
-    $monitor("time=%0t: current=%h, spike_out=%h, threshold=%h", $time, ui_in, uo_out, uio_out);
-    $dumpfile("tb.vcd"); 
-    $dumpvars(0, LSNN_tb);    
-end
+    initial begin
+        $monitor("time=%0t: current=%h, spike_out=%h, threshold=%h", $time, ui_in, uo_out, uio_out);
+        $dumpfile("tb.vcd"); 
+        $dumpvars(0, LSNN_tb);    
+    end
 
 endmodule
